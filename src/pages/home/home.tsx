08afd44468464f38549155ab50fb28986eae2e5d
henrique945 <henrique.rodrigues7@hotmail.com>
2021-01-28 12:29:52 -0300

    create(interfaces): create movie, home-data and tmdb interfaces

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,16 @@
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { HomeDataInterface } from '../../interfaces/home-data.interface';
 import Tmdb from '../../services/tmdb';
 
 function Home(): ReactElement {
+
+  const [movieList, setMovieList] = useState<HomeDataInterface[]>([]);
 
   useEffect(() => {
     const loadAll = async () => {
       // pegando a lista total
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
     };
 
     loadAll();
