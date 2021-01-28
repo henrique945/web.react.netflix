2e72b38df14161a0febf466a917db6eb07dc3392
henrique945 <henrique.rodrigues7@hotmail.com>
2021-01-28 14:00:33 -0300

    update(movie-row): styled and animated with transitions

--- src/components/movie-row/movie-row.tsx
+++ src/components/movie-row/movie-row.tsx
@@ -4,12 +4,16 @@ import { MovieRowInterface } from './movie-row.interface';
 
 export default ({ title, items }: MovieRowInterface) => {
   return (
-    <div>
+    <div className="movie-row">
       <h2>{ title }</h2>
-      <div className="movie-row--list">
-        { items.results.length > 0 && items.results.map((item, key) => (
-          <img src={ `https://image.tmdb.org/t/p/w300${ item.poster_path }` } alt="Imagem do filme"/>
-        )) }
+      <div className="movie-row--list-area">
+        <div className="movie-row--list">
+          { items.results.length > 0 && items.results.map((item, key) => (
+            <div key={key} className="movie-row--item">
+              <img src={ `https://image.tmdb.org/t/p/w300${ item.poster_path }` } alt="Imagem do filme"/>
+            </div>
+          )) }
+        </div>
       </div>
     </div>
   );
